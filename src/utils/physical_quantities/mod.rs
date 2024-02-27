64d2887ca502f814a163d2ead2ef64161cf972f9
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-02-27 10:12:24 +0100

    🔥 Change the color module making it re-export the embedded_graphics::pixelcolor instead of having my custom struct

--- src/utils/physical_quantities/mod.rs
+++ src/utils/physical_quantities/mod.rs
@@ -1,5 +1,6 @@
-//! This module provides structs for representing different physical quantities, such as colors, durations, frequencies...
+//! This module provides structs for representing different physical quantities, such as temperatures, durations, frequencies...
 
+#[cfg(feature = "graphics")]
 pub mod color;
 pub mod duration;
 pub mod frequency;
