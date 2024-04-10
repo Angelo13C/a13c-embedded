5db8b5f6d9aff4b8741152a01a97428dcd0f9744
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-04-10 16:39:51 +0200

    ✨ Add a struct for a led driver

---
+++ src/drivers/led/mod.rs
@@ -0,0 +1,47 @@
+use embedded_hal::digital::{ErrorType, OutputPin, StatefulOutputPin};
+
+pub struct Led<P: OutputPin>
+{
+	pin: P,
+}
+
+impl<P: OutputPin> Led<P>
+{
+	pub fn new(pin: P) -> Self
+	{
+		Self { pin }
+	}
+
+	pub fn turn_on(&mut self) -> Result<(), <P as ErrorType>::Error>
+	{
+		self.pin.set_high()
+	}
+	pub fn turn_off(&mut self) -> Result<(), <P as ErrorType>::Error>
+	{
+		self.pin.set_low()
+	}
+	pub fn set_to(&mut self, on: bool) -> Result<(), <P as ErrorType>::Error>
+	{
+		if on
+		{
+			self.turn_on()
+		}
+		else
+		{
+			self.turn_off()
+		}
+	}
+}
+
+impl<P: StatefulOutputPin> Led<P>
+{
+	pub fn toggle(&mut self) -> Result<(), <P as ErrorType>::Error>
+	{
+		self.pin.toggle()
+	}
+
+	pub fn is_on(&mut self) -> Result<bool, <P as ErrorType>::Error>
+	{
+		self.pin.is_set_high()
+	}
+}
