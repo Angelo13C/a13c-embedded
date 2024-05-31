a10de56ea6c655afab75be70da56ad59cf572758
AngeloCipriani <angelo13cipriani.03@gmail.com>
2024-05-31 15:14:21 +0200

    ✨ Extend functionalities of math vectors (from micromath) thanks to a trait

--- src/utils/math/micromath.rs
+++ src/utils/math/micromath.rs
@@ -1,2 +1,25 @@
-//! This module just re-exports the `micromath` crate.
+//! This module re-exports the `micromath` crate and adds a bit of functionality to its content.
+
 pub use micromath;
+use micromath::vector::Vector2d;
+
+use super::shapes::Component;
+
+pub trait VectorExt<C: Component>
+{
+	fn length(&self) -> C;
+	fn length_squared(&self) -> C;
+}
+
+impl<C: Component> VectorExt<C> for Vector2d<C>
+{
+	fn length(&self) -> C
+	{
+		self.length_squared().sqrt()
+	}
+
+	fn length_squared(&self) -> C
+	{
+		self.x * self.x + self.y * self.y
+	}
+}
